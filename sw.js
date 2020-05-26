510be5a305b5e0fb187c750b49de2a30dbd8ee65
Poornachandra C M <50172076+Poorna1396@users.noreply.github.com>
2020-05-26 18:37:35 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,33 @@
+const cacheContents = 'cachedPages'
+const asset = [
+    '/',
+    '/index.html',
+    '/welcome.html',
+    '/failure.html',
+    '/manifest.json',
+    '/sw.js',
+]
+
+self.addEventListener('install', event=>{
+    console.log("worker installed");
+    event.waitUntil(
+        caches.open(cacheContents).then(cache =>{
+            console.log("caching assets");
+            cache.addAll(asset);
+        })
+    );
+
+});
+
+self.addEventListener('activate', event=>{
+    console.log("worker activated");
+});
+
+self.addEventListener('fetch', event=>{
+    console.log("fetch fired ",event);
+    event.respondWith(
+        caches.match(event.request).then(res =>{
+            return res || fetch(event.request);
+        })
+    );
+});
